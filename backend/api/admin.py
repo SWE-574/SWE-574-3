522b1ce3bb2747df621012a23dbaa5671d6b0165
yaseminsirin <yaseminsirin322@gmail.com>
2026-03-30 00:20:53 +0300

    feat(follow): add active user follow relationship model

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import UserFollow
+
+
+@admin.register(UserFollow)
+class UserFollowAdmin(admin.ModelAdmin):
+    list_display = ('id', 'follower', 'following', 'created_at')
+    readonly_fields = ('created_at',)
